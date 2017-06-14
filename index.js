30fa82391318dc294f3aefd834c3453237a65ec4
Marius Ciotlos <marius.ciotlos@betfair.com>
2017-06-14 13:01:16 +0300

    Made more generic, added better console status

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 require("./lib/config.js").init(); // Load configuration
+require("./lib/status.js").init(); // Better console
 require("./lib/ble.js"); // Enable Advertising packet discovery
